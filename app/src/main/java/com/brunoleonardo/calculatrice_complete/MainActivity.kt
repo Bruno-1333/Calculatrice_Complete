f41d892984548ac31913ff54da745fb3105752eb
Bruno-1333 <2295132@cegepoutaouais.qc.ca>
2023-05-29 13:48:38 -0400

    version final

--- app/src/main/java/com/brunoleonardo/calculatrice_complete/MainActivity.kt
+++ app/src/main/java/com/brunoleonardo/calculatrice_complete/MainActivity.kt
@@ -2,6 +2,7 @@ package com.brunoleonardo.calculatrice_complete
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import com.brunoleonardo.calculatrice_complete.databinding.ActivityMainBinding
 
@@ -20,4 +21,70 @@ class MainActivity : AppCompatActivity() {
         var b : Button = view as Button // on récupère le bouton qui a été cliqué
         binding.textViewResultat.text = binding.textViewResultat.text.toString() + b.text.toString()
     }
+
+    fun actionC(view: android.view.View) {
+        binding.textViewResultat.text = "" // on efface le contenu du TextView
+        binding.textViewAfficherResultat.text = "" // on efface le contenu du TextView
+    }
+
+    fun actionCE(view: android.view.View) {
+        var contenu = binding.textViewResultat.text.toString() // on récupère le contenu du TextView
+        if (contenu.length > 0) { // si le contenu du TextView est plus grand que 0
+            contenu = contenu.substring(0, contenu.length - 1) // on enlève le dernier caractère
+            binding.textViewResultat.text = contenu // on affiche le nouveau contenu du TextView
+        }
+    }
+
+    fun actionAdition(view: android.view.View) {
+        binding.textViewResultat.text = binding.textViewResultat.text.toString() + "+"
+    }
+
+    fun actionSoustraction(view: android.view.View) {
+        binding.textViewResultat.text = binding.textViewResultat.text.toString() + "-"
+    }
+
+    fun actionMultiplication(view: android.view.View) {
+        binding.textViewResultat.text = binding.textViewResultat.text.toString() + "*"
+    }
+
+    fun actionDivision(view: android.view.View) {
+        binding.textViewResultat.text = binding.textViewResultat.text.toString() + "/"
+    }
+
+    // fonction qui est appelée quand on clique sur le bouton =
+    fun actionEgal(view: android.view.View){
+        // on récupère le contenu du TextView
+        var contenu = binding.textViewResultat.text.toString()
+        // on initialise la variable resultat
+        var resultat = 0.0
+        // on vérifie si le contenu du TextView contient le caractère "+"
+        if (contenu.contains("+")) {
+            // on découpe le contenu du TextView en deux parties
+            var tab = contenu.split("+")
+            // on additionne les deux parties
+            resultat = tab[0].toDouble() + tab[1].toDouble()
+            // on vérifie si le contenu du TextView contient le caractère "-"
+        } else if (contenu.contains("-")) {
+            // on découpe le contenu du TextView en deux parties
+            var tab = contenu.split("-")
+            // on soustrait les deux parties
+            resultat = tab[0].toDouble() - tab[1].toDouble()
+            // on vérifie si le contenu du TextView contient le caractère "*"
+        } else if (contenu.contains("*")) {
+            // on découpe le contenu du TextView en deux parties
+            var tab = contenu.split("*")
+            // on multiplie les deux parties
+            resultat = tab[0].toDouble() * tab[1].toDouble()
+            // on vérifie si le contenu du TextView contient le caractère "/"
+        } else if (contenu.contains("/")) {
+            // on découpe le contenu du TextView en deux parties
+            var tab = contenu.split("/")
+            // on divise les deux parties
+            resultat = tab[0].toDouble() / tab[1].toDouble()
+
+        }
+        // on affiche le résultat dans le TextView
+        binding.textViewAfficherResultat.text = resultat.toString()
+    }
+
 }
